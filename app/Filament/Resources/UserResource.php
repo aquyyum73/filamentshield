301642cc050ee00b1b1c9c41c246ac38bde1909d
aquyyum73 <aquyyum73@gmail.com>
2023-11-27 13:05:45 +0500

    Downloded to Office PC

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'fab-windows';
+    protected static ?string $navigationGroup = 'Settings';
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(191),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(191),
+                //Forms\Components\DateTimePicker::make('email_verified_at'),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(191),
+                Forms\Components\Select::make('role')
+                    ->relationship('roles' , 'name')
+                    ->multiple()
+                    ->preload()
+                    ->searchable(),
+                SpatieMediaLibraryFileUpload::make('avatar')
+                    ->label('User Picture')
+                    ->collection('users')
+                    ->directory('users')
+                    ->image()
+                    ->imageEditor(),
+                // Forms\Components\TextInput::make('theme')
+                //     ->maxLength(191)
+                //     ->default('default'),
+                // Forms\Components\TextInput::make('theme_color')
+                //     ->maxLength(191),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                SpatieMediaLibraryImageColumn::make('avatar')
+                    ->label('User Picture')
+                    ->collection('users')
+                    ->circular()
+                    ->size(60),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                // Tables\Columns\TextColumn::make('email_verified_at')
+                //     ->dateTime()
+                //     ->sortable(),
+                Tables\Columns\TextColumn::make('roleName')
+                    ->label('Roles')
+                    ->formatStateUsing(fn ($state): string => Str::headline($state))
+                    ->badge()
+                    ->separator(','),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                // Tables\Columns\TextColumn::make('theme')
+                //     ->searchable(),
+                // Tables\Columns\TextColumn::make('theme_color')
+                //     ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                    Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
